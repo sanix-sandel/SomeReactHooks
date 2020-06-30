7d79d6f79ffb3c3018a6f7024c2a363cbde0c474
sanix sandel <sanicksikani@gmail.com>
2020-06-30 20:34:17 +0300

    useState hooks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-
+//import ClassCounter from './components/ClassCounter.js';
+import HookCounter from './components/HookCounter.js';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <HookCounter/>
     </div>
   );
 }

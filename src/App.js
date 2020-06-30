9ca27304160219ce90dec7386a133f5e4f44d3d7
sanix sandel <sanicksikani@gmail.com>
2020-06-30 20:53:34 +0300

    useState with previous state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 //import ClassCounter from './components/ClassCounter.js';
-import HookCounter from './components/HookCounter.js';
+//import HookCounter from './components/HookCounter.js';
+import HookCounterTwo from './components/HookCounterTwo.js';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <HookCounter/>
+      <HookCounterTwo/>
     </div>
   );
 }
